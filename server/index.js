cf99bf14d4981660c9ab78fa943ba39fa1485f0e
mstoliar <mstoliar@softkitit.com>
2023-05-03 16:18:29 +0300

    create modules

--- server/index.js
+++ server/index.js
@@ -1,6 +1,7 @@
 require('dotenv').config()
 const express = require('express')
 const sequelize = require('./db')
+const modules = require('./modules/module')
 
 const PORT = process.env.PORT || 5000
 
